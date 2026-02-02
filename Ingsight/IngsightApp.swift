0f425fb5ee5929867379216af937633efcd267f9
Talha Fırat <148004819+tfmese@users.noreply.github.com>
2026-02-02 16:57:40 +0300

    Initial Commit

---
+++ Ingsight/IngsightApp.swift
@@ -0,0 +1,32 @@
+//
+//  IngsightApp.swift
+//  Ingsight
+//
+//  Created by Talha Fırat on 2.02.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct IngsightApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ IngsightTests/IngsightTests.swift
@@ -0,0 +1,17 @@
+//
+//  IngsightTests.swift
+//  IngsightTests
+//
+//  Created by Talha Fırat on 2.02.2026.
+//
+
+import Testing
+@testable import Ingsight
+
+struct IngsightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
